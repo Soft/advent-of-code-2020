744186fad35d6549cd5fbc58119b10459c119b68
Samuel Laurén <samuel.lauren@iki.fi>
2020-12-10 18:55:48 +0200

    Add day 10 part 2

--- aoc/days/day10.py
+++ aoc/days/day10.py
@@ -1,4 +1,6 @@
 from collections import Counter
+from itertools import count
+from functools import lru_cache
 
 
 def jolts(input):
@@ -19,3 +21,28 @@ def sliding_pairs(iterable):
 def part_1(input):
     differences = Counter(b - a for a, b in sliding_pairs(jolts(input)))
     print(differences[1] * differences[3])
+
+
+def neighbors(jolts, index):
+    current = jolts[index]
+    for i in count(index + 1):
+        if i == len(jolts):
+            return
+        if jolts[i] - current <= 3:
+            yield i
+        else:
+            return
+
+
+def traverse_paths(jolts):
+    @lru_cache(None)
+    def traverse(index):
+        if jolts[index] == jolts[-1]:
+            return 1
+        return sum(traverse(i) for i in neighbors(jolts, index))
+
+    return traverse(0)
+
+
+def part_2(input):
+    print(traverse_paths(jolts(input)))
