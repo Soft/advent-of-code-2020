a52c7b0dcb993046eed7ac3467013cb79ea0f676
Samuel Laurén <samuel.lauren@iki.fi>
2020-12-11 19:43:19 +0200

    Add day 11 part 2

--- aoc/days/day11.py
+++ aoc/days/day11.py
@@ -1,9 +1,28 @@
-from itertools import product
-from collections import namedtuple
 from copy import deepcopy
+from dataclasses import dataclass
+from functools import partial
+from itertools import count, product, takewhile
+from typing import List
 
 
-class Room(namedtuple("Room", ("layout", "width", "height"))):
+def takewhile1(pred, iterable):
+    """
+    variant of takewhile that also returns the first item where pred does not
+    return True.
+    """
+    for item in iterable:
+        ok = pred(item)
+        yield item
+        if not ok:
+            break
+
+
+@dataclass
+class Room:
+    layout: List[List[str]]
+    width: int
+    height: int
+
     @classmethod
     def parse(cls, input):
         layout = list(map(list, input.splitlines()))
@@ -29,35 +48,68 @@ class Room(namedtuple("Room", ("layout", "width", "height"))):
             (x - 1, y + 1),
             (x - 1, y),
         ]
-        return ((x, y) for x, y in adjacent if self.inside(x, y))
+        return filter(self.inside, adjacent)
 
-    def inside(self, x, y):
+    def visible(self, x, y):
+        def floor(pos):
+            x, y = pos
+            return self.tile(x, y) == "."
+
+        def visible_tiles(coords):
+            return takewhile1(floor, takewhile(self.inside, coords))
+
+        yield from visible_tiles((x - n, y - n) for n in count(1))
+        yield from visible_tiles((x, y - n) for n in count(1))
+        yield from visible_tiles((x + n, y - n) for n in count(1))
+        yield from visible_tiles((x + n, y) for n in count(1))
+        yield from visible_tiles((x + n, y + n) for n in count(1))
+        yield from visible_tiles((x, y + n) for n in count(1))
+        yield from visible_tiles((x - n, y + n) for n in count(1))
+        yield from visible_tiles((x - n, y) for n in count(1))
+
+    def inside(self, pos):
+        x, y = pos
         return 0 <= x < self.width and 0 <= y < self.height
 
+    def occupied_seats(self):
+        return sum(self.tile(nx, ny) == "#" for nx, ny in self.positions())
 
-def occupied(room):
-    return sum(room.tile(nx, ny) == "#" for nx, ny in room.positions())
+    def occupied_neighbors(self, x, y):
+        return sum(self.tile(nx, ny) == "#" for nx, ny in self.neighbors(x, y))
+
+    def occupied_visible(self, x, y):
+        return sum(self.tile(nx, ny) == "#" for nx, ny in self.visible(x, y))
 
 
-def occupied_neighbors(room, x, y):
-    return sum(room.tile(nx, ny) == "#" for nx, ny in room.neighbors(x, y))
-
-
-def step(room):
+def step(vacant, occupied, room):
     next = deepcopy(room)
     for x, y in room.positions():
         cell = room.tile(x, y)
-        if cell == "L" and occupied_neighbors(room, x, y) == 0:
+        if cell == "L" and vacant(room, x, y):
             next.set_tile(x, y, "#")
-        elif cell == "#" and occupied_neighbors(room, x, y) >= 4:
+        elif cell == "#" and occupied(room, x, y):
             next.set_tile(x, y, "L")
     return next
 
 
+def until_stable(fn, input):
+    prev = object()
+    current = input
+    while prev != current:
+        prev = current
+        current = fn(current)
+    return current
+
+
 def part_1(input):
-    prev = None
-    state = Room.parse(input)
-    while prev != state:
-        prev = state
-        state = step(state)
-    print(occupied(state))
+    vacant = lambda room, x, y: room.occupied_neighbors(x, y) == 0
+    occupied = lambda room, x, y: room.occupied_neighbors(x, y) >= 4
+    stepper = partial(step, vacant, occupied)
+    print(until_stable(stepper, Room.parse(input)).occupied_seats())
+
+
+def part_2(input):
+    vacant = lambda room, x, y: room.occupied_visible(x, y) == 0
+    occupied = lambda room, x, y: room.occupied_visible(x, y) >= 5
+    stepper = partial(step, vacant, occupied)
+    print(until_stable(stepper, Room.parse(input)).occupied_seats())
