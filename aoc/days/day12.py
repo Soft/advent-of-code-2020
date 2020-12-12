a8052e8090a98516507786b5760816b28c29588c
Samuel Laurén <samuel.lauren@iki.fi>
2020-12-12 13:58:39 +0200

    Day 12 part 2

--- aoc/days/day12.py
+++ aoc/days/day12.py
@@ -15,11 +15,11 @@ def angle_to_dir(angle):
     return ["E", "N", "W", "S"][int(angle / 90 % 4)]
 
 
-def step(action, angle, x, y):
+def step1(action, angle, x, y):
     if action.type == "N":
-        return angle, x, y - action.value
-    elif action.type == "S":
         return angle, x, y + action.value
+    elif action.type == "S":
+        return angle, x, y - action.value
     elif action.type == "E":
         return angle, x + action.value, y
     elif action.type == "W":
@@ -30,14 +30,14 @@ def step(action, angle, x, y):
         return angle - action.value, x, y
     elif action.type == "F":
         act = Action(angle_to_dir(angle), action.value)
-        return step(act, angle, x, y)
+        return step1(act, angle, x, y)
     assert False
 
 
-def move(actions, angle, x, y):
+def move(fn, actions, *args):
     for action in actions:
-        angle, x, y = step(action, angle, x, y)
-    return angle, x, y
+        args = fn(action, *args)
+    return args
 
 
 def distance(x1, y1, x2, y2):
@@ -46,5 +46,42 @@ def distance(x1, y1, x2, y2):
 
 def part_1(input):
     angle, x1, y1 = 0, 0, 0
-    _, x2, y2 = move(actions(input), angle, x1, y1)
+    _, x2, y2 = move(step1, actions(input), angle, x1, y1)
     print(distance(x1, y1, x2, y2))
+
+
+def rotate(angle, wx, wy):
+    if angle == 0:
+        return wx, wy
+    elif angle == 90:
+        return wy, -wx
+    elif angle == 180:
+        return -wx, -wy
+    elif angle == 270:
+        return -wy, wx
+    assert False
+
+
+def step2(action, wx, wy, sx, sy):
+    if action.type == "N":
+        return wx, wy + action.value, sx, sy
+    elif action.type == "S":
+        return wx, wy - action.value, sx, sy
+    elif action.type == "E":
+        return wx + action.value, wy, sx, sy
+    elif action.type == "W":
+        return wx - action.value, wy, sx, sy
+    elif action.type == "L":
+        return (*rotate(360 - action.value, wx, wy), sx, sy)
+    elif action.type == "R":
+        return (*rotate(action.value, wx, wy), sx, sy)
+    elif action.type == "F":
+        return wx, wy, sx + (wx * action.value), sy + (wy * action.value)
+    assert False
+
+
+def part_2(input):
+    sx, sy = 0, 0
+    wx, wy = 10, 1
+    _, _, sx2, sy2 = move(step2, actions(input), wx, wy, sx, sy)
+    print(distance(sx, sy, sx2, sy2))
