4bd737a3260fbb86fc9ef04f20ad43bd52d92742
Samuel Laurén <samuel.lauren@iki.fi>
2020-12-14 20:15:31 +0200

    Day 14 part 2

--- aoc/days/day14.py
+++ aoc/days/day14.py
@@ -1,8 +1,10 @@
 import re
 from dataclasses import dataclass
+from itertools import product
 
 MASK_RE = re.compile(r"mask = ([01X]+)")
 STORE_RE = re.compile(r"mem\[(\d+)\] = (\d+)")
+MAX_ADDRESS = 2 ** 36 - 1
 
 
 @dataclass
@@ -13,21 +15,20 @@ class Store:
 
 @dataclass
 class Mask:
-    and_mask: str
-    or_mask: str
+    value: str
 
 
 def parse_mask(value):
-    and_mask = int(re.sub("[X1]", "1", value), 2)
-    or_mask = int(re.sub("[X0]", "0", value), 2)
-    return Mask(and_mask, or_mask)
+    and_mask = int(re.sub("X", "1", value), 2)
+    or_mask = int(re.sub("X", "0", value), 2)
+    return and_mask, or_mask
 
 
 def instructions(input):
     for line in input.splitlines():
         mask = MASK_RE.fullmatch(line)
         if mask is not None:
-            yield parse_mask(mask.group(1))
+            yield Mask(mask.group(1))
             continue
         store = STORE_RE.fullmatch(line)
         if store is not None:
@@ -36,16 +37,15 @@ def instructions(input):
         assert False
 
 
-def evaluate(instructions):
+def evaluate1(instructions):
     mem = {}
-    mask = None
     for instruction in instructions:
         if isinstance(instruction, Mask):
-            mask = instruction
+            and_mask, or_mask = parse_mask(instruction.value)
         elif isinstance(instruction, Store):
             value = instruction.value
-            value &= mask.and_mask
-            value |= mask.or_mask
+            value &= and_mask
+            value |= or_mask
             mem[instruction.address] = value
         else:
             assert False
@@ -53,4 +53,43 @@ def evaluate(instructions):
 
 
 def part_1(input):
-    print(sum(evaluate(instructions(input)).values()))
+    print(sum(evaluate1(instructions(input)).values()))
+
+
+def expand_floating_masks(mask):
+    indices = tuple(ind for ind, b in enumerate(reversed(mask)) if b == "X")
+    for seq in product((True, False), repeat=len(indices)):
+        and_mask = MAX_ADDRESS
+        or_mask = 0
+        for i, b in zip(indices, seq):
+            if b:
+                or_mask |= 1 << i
+            else:
+                and_mask &= ~(1 << i)
+        yield and_mask, or_mask
+
+
+def decode_address(mask, address):
+    address |= int(re.sub("X", "0", mask), 2)
+    for and_mask, or_mask in expand_floating_masks(mask):
+        addr = address
+        addr &= and_mask
+        addr |= or_mask
+        yield addr
+
+
+def evaluate2(instructions):
+    mem = {}
+    for instruction in instructions:
+        if isinstance(instruction, Mask):
+            mask = instruction.value
+        elif isinstance(instruction, Store):
+            for address in decode_address(mask, instruction.address):
+                mem[address] = instruction.value
+        else:
+            assert False
+    return mem
+
+
+def part_2(input):
+    print(sum(evaluate2(instructions(input)).values()))
