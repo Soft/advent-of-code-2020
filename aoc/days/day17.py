9d056062abf684f6b6d09875a12db699d94a3fb0
Samuel Laurén <samuel.lauren@iki.fi>
2020-12-17 13:43:16 +0200

    Day 17 part 2

--- aoc/days/day17.py
+++ aoc/days/day17.py
@@ -1,32 +1,40 @@
 from collections import defaultdict
-from itertools import product
+from itertools import product, starmap
+from operator import add
 
-OFFSETS = frozenset(off for off in product((-1, 0, 1), repeat=3))
-NEIGHBOR_OFFSETS = frozenset(off for off in OFFSETS if off != (0, 0, 0))
+OFFSETS_3D = frozenset(off for off in product((-1, 0, 1), repeat=3))
+OFFSETS_4D = frozenset(off for off in product((-1, 0, 1), repeat=4))
 
 
-def parse(input):
+def pad(source, size):
+    assert len(source) <= size
+    dest = [0] * size
+    dest[: len(source)] = source
+    return tuple(dest)
+
+
+def parse(input, dim=3):
     world = defaultdict(lambda: False)
     for y, row in enumerate(input.splitlines()):
         for x, col in enumerate(row):
             if col == "#":
-                world[(x, y, 0)] = True
+                world[pad((x, y), dim)] = True
     return world
 
 
-def neighbors(x, y, z):
-    yield from ((x + x1, y + y1, z + z1) for x1, y1, z1 in NEIGHBOR_OFFSETS)
+def perimeter(pos, *, offsets=OFFSETS_3D):
+    yield from (tuple(starmap(add, zip(pos, off))) for off in offsets)
 
 
-def perimeter(x, y, z):
-    yield from ((x + x1, y + y1, z + z1) for x1, y1, z1 in OFFSETS)
+def neighbors(pos, *, offsets=OFFSETS_3D):
+    yield from (p for p in perimeter(pos, offsets=offsets) if p != pos)
 
 
-def step(world):
+def step(world, offsets=OFFSETS_3D):
     next = world.copy()
-    cells = {c for pos in world for c in perimeter(*pos)}
+    cells = {c for pos in world for c in perimeter(pos, offsets=offsets)}
     for cell in cells:
-        active_neighbors = sum(world[pos] for pos in neighbors(*cell))
+        active_neighbors = sum(world[pos] for pos in neighbors(cell, offsets=offsets))
         if world[cell]:
             if active_neighbors not in (2, 3):
                 next[cell] = False
@@ -36,10 +44,10 @@ def step(world):
     return next
 
 
-def simulate(world, steps=6):
+def simulate(world, offsets=OFFSETS_3D, steps=6):
     current = world
     for _ in range(steps):
-        current = step(current)
+        current = step(current, offsets=offsets)
     return current
 
 
@@ -50,3 +58,8 @@ def active(world):
 def part_1(input):
     world = parse(input)
     print(active(simulate(world)))
+
+
+def part_2(input):
+    world = parse(input, dim=4)
+    print(active(simulate(world, OFFSETS_4D)))
