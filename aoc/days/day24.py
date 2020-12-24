5d6d5e7ec29ecd2d49b208e520e4c7b685fc09b9
Samuel Laurén <samuel.lauren@iki.fi>
2020-12-24 11:25:14 +0200

    Day 24 part 2

--- aoc/days/day24.py
+++ aoc/days/day24.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from dataclasses import dataclass
 from functools import reduce
 from operator import add
+from itertools import chain, count, islice
 
 
 @dataclass(frozen=True)
@@ -60,5 +61,39 @@ def tile(paths):
     return room
 
 
+def black(room):
+    return sum(room.values())
+
+
 def part_1(input):
-    print(sum(tile(parse(input)).values()))
+    print(black(tile(parse(input))))
+
+
+def neighbors(pos):
+    return (pos + dir for dir in DIRECTIONS.values())
+
+
+def step(room):
+    next = room.copy()
+    tiles = {p for pos in room for p in chain(neighbors(pos), (pos,))}
+    for tile in tiles:
+        black = sum(room[pos] for pos in neighbors(tile))
+        if room[tile]:
+            if black == 0 or black > 2:
+                next[tile] = False
+        else:
+            if black == 2:
+                next[tile] = True
+    return next
+
+
+def iterate(fn, value):
+    return (value := fn(value) for _ in count())
+
+
+def nth(iter, n):
+    return next(islice(iter, n - 1, n))
+
+
+def part_2(input):
+    print(black(nth(iterate(step, tile(parse(input))), 100)))
